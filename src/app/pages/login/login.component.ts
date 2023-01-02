fec12252be0cbf21127da45c8347ebf73076c204
Ouijdaneel25 <ouijdaneelazri25@gmail.com>
2023-01-02 01:37:07 +0100

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthentificationJWTService } from 'src/app/service/authentification-jwt.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,48 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
 
+  constructor(private service: AuthentificationJWTService,private tokenService: TokenStorageService) { }
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  login = {
+    username: '',
+    password: ''
+  };
   ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenService.getUser().roles;
+    }
   }
+
   ngOnDestroy() {
   }
+  Login() {
+    const data2 = {
+      username: this.login.username,
+      password: this.login.password,
+    };
+    this.service.login(data2.username, data2.password).subscribe(
+      data => {
+        this.tokenService.saveToken(data.accessToken);
+        this.tokenService.saveUser(data);
 
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenService.getUser().roles;
+       // this.reloadPage();
+       alert("bien login")
+       console.log(data.accessToken)
+       console.log(data)
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
 }
--- src/app/service/authentification-jwt.service.ts
+++ src/app/service/authentification-jwt.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,37 +11,46 @@ import Swal from 'sweetalert2';
 export class AuthentificationJWTService {
 
   constructor(private http: HttpClient) { }
-  async save(role: any,username:any,age:any,password,genre:any) {
+  async save(role: any, username: any, age: any, password, genre: any) {
     try {
-      const  req  = await this.http.post('http://localhost:8900/SPORTIFYAUTHENTIFICATION/auth/save_user', {
+      const req = await this.http.post('http://localhost:8900/SPORTIFYAUTHENTIFICATION/auth/save_user', {
         username: username,
-        age:age,
-        genre:genre,
+        age: age,
+        genre: genre,
         password: password,
         role: role,
       }).subscribe(_ => {
         console.log(username)
         console.log(role)
         console.log("ouijdane")
-          return this.http.post('http://localhost:8900/SPORTIFYAUTHENTIFICATION/auth/addRoleToUser', role).toPromise();
-          
-       }, error => 
-       Swal.fire({
-        icon: 'error',
-        title: 'this account  already exists please change it',
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
-        },
-      })
+        return this.http.post('http://localhost:8900/SPORTIFYAUTHENTIFICATION/auth/addRoleToUser', role).toPromise();
 
-       )
+      }, error =>
+        Swal.fire({
+          icon: 'error',
+          title: 'this account  already exists please change it',
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          },
+        })
+
+      )
     } catch (error) {
       console.log('An error occurred while registering');
     }
   }
 
+  login(username: string, password: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    return this.http.post("http://localhost:8900/SPORTIFYAUTHENTIFICATION/login", {
+      username,
+      password
+    }, httpOptions);
+  }
 
 }
